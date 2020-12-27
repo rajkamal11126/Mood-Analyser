8e328fc9c9ba6c554320dffac71c5000a28a7a9c
Raj <rajkamal11126@gmail.com>
2020-12-27 22:56:34 +0530

    Added: Functionality to add test case of no such class

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -2,22 +2,25 @@ package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser {
 	private String message;
-    public MoodAnalyser() {
-    }
 
-    public MoodAnalyser(String message) {
-        this.message = message;
-    }
+	public MoodAnalyser() {
+	}
 
-    public String analyseMood() throws MoodAnalysisException {
-        try {
-            if (message.length()==0)
-                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY,"Please enter proper mood");
-            if (message.contains("sad"))
-                return "SAD";
-            return "HAPPY";
-        } catch (NullPointerException e) {
-            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL,"Please enter proper mood");
-        }
-    }
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+
+	public String analyseMood() throws MoodAnalysisException {
+		try {
+			if (message.length() == 0)
+				throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY,
+						"Please enter proper mood");
+			if (message.contains("Sad"))
+				return "SAD";
+			return "HAPPY";
+		} catch (NullPointerException e) {
+			throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL,
+					"Please enter proper mood");
+		}
+	}
 }
--- src/test/java/com/bridgelabz/moodanalysertest/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalysertest/MoodAnalyserTest.java
@@ -4,43 +4,83 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.bridgelabz.moodanalyser.MoodAnalyser;
+import com.bridgelabz.moodanalyser.MoodAnalyserFactory;
 import com.bridgelabz.moodanalyser.MoodAnalysisException;
 
 public class MoodAnalyserTest {
-	 
-	    @Test
-	    public void givenMessage_WhenNotSad_ShoulReturnHappy() {
-	        MoodAnalyser moodAnalyser = new MoodAnalyser("I am happy");
-	        String mood;
-			try {
-				mood = moodAnalyser.analyseMood();
-				Assert.assertEquals("HAPPY", mood);
-			} catch (MoodAnalysisException e) {
-				e.printStackTrace();
-			} 
-	    }
-	    @Test
-	    public void givenMessage_WhenSad_ShouldReturnSad() {
-	        MoodAnalyser moodAnalyser = new MoodAnalyser("I am sad");
-	        String mood;
-			try {
-				mood = moodAnalyser.analyseMood();
-				Assert.assertEquals("SAD", mood);
-			} catch (MoodAnalysisException e) {
-				e.printStackTrace();
-			}      
-	    }
-	    
-	    @Test
-	    public void givenNullMood_ShouldReturnHappy() {
-	        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-	        String mood;
-			try {
-				mood = moodAnalyser.analyseMood();
-				Assert.assertEquals("HAPPY" , mood);
-			} catch (MoodAnalysisException e) {
-				e.printStackTrace();
-			}
-	        
-	    }
+
+	@Test
+    public void givenMessage_WhenSad_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+        String mood = null;
+        try {
+            mood = moodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void givenMessage_WhenHappy_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
+        String mood = null;
+        try {
+            mood = moodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals("HAPPY", mood);
+    }
+
+    @Test
+    public void givenNullMood_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        String mood = null;
+        try {
+            mood = moodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals("HAPPY", mood);
+    }
+
+    @Test
+    public void givenEmptyMood_ShouldThrowException_withType() {
+        MoodAnalyser realMoodAnalyser = new MoodAnalyser("");
+        try {
+            realMoodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+            Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_EMPTY,e.type);
+        }
+    }
+
+    @Test
+    public void givenNullMood_ShouldThrowException_withType() {
+        MoodAnalyser realMoodAnalyser = new MoodAnalyser(null);
+        try {
+            realMoodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+            Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL,e.type);
+        }
+    }
+
+    @Test
+    public void givenMoodAnalyser_WhenImproper_shouldReturnDefaultObject() {
+          MoodAnalyser moodAnalyser = MoodAnalyserFactory.createMoodAnalyser();
+          boolean result = moodAnalyser.equals(new MoodAnalyser());
+          Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenMoodAnalyser_WhenImproper_shouldReturn_withNoSuchClass() {
+       try {
+           MoodAnalyser moodAnalyser = MoodAnalyserFactory.createMoodAnalyserObject_NoClassFound_OR_NoMethodFound();
+       }catch (MoodAnalysisException e){
+           Assert.assertEquals("NO_SUCH_CLASS_ERROR",e.getMessage());
+       }
+    }
+    
 }
